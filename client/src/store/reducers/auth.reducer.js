88af597fa97f4a26f018793b6022ec5615ad32ea
Kyle Kelso <kyle.kelso@outlook.com>
2019-09-30 12:28:06 -0400

    updated redux store

--- client/src/store/reducers/auth.reducer.js
+++ client/src/store/reducers/auth.reducer.js
@@ -4,18 +4,23 @@ import reduxHelper from "./../../helpers/reduxHelper";
 const login = reduxHelper(actionTypes.LOGIN);
 const session = reduxHelper(actionTypes.SESSION);
 const logout = reduxHelper(actionTypes.LOGOUT);
+const join = reduxHelper(actionTypes.JOIN);
 
 export default function(state, action) {
   let newState = login.reducer(state, action, { isAuthenticated: false });
+  newState = join.reducer(newState, action);
   newState = session.reducer(newState, action);
   newState = logout.reducer(newState, action);
 
   switch (action.type) {
     case login.types.request:
+    case join.types.request:
       return { ...newState, isAuthenticated: false };
     case login.types.failure:
+    case join.types.failure:
       return { ...newState, isAuthenticated: false };
     case login.types.success:
+    case join.types.success:
       return {
         ...newState,
         isAuthenticated: true
